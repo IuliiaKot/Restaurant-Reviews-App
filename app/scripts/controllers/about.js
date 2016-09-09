6d436420cdb7283358629bb05cd03295cea36c27
IuliiaKot <kotlenko.julia@gmail.com>
2016-09-09 13:34:43 -0400

    init empty sceleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reviewAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reviewAppApp
+ */
+angular.module('reviewAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reviewAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reviewAppApp
+ */
+angular.module('reviewAppApp')
+  .controller('MainCtrl', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.title = 'reviewApp'
+    $scope.
+  });
