76292d34088c14b5f4a16ab260e1651e0193290c
IuliiaKot <kotlenko.julia@gmail.com>
2016-10-19 00:39:37 -0400

    add filter by rating

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,24 +31,69 @@ angular.module('reviewAppApp')
     fetchData();
     $scope.title = 'reviewApp';
 
-    $scope.findPlace = function(name){
-      var inputValueCuisine = ($scope.selectedItem ? $scope.selectedItem.name : "");
-      var inputStar = $scope.selectedStar;
-      if (inputValueCuisine !== 'All' ){
-        $http.get('../data/restaurant.json').success(function(data){
-          $scope.restaurants = data.restaurants.filter((restaurant) => {
-            return restaurant.restaurant.cuisines.includes(inputValueCuisine);
-          });
+    function helper(cuisine, star){
+      $http.get('../data/restaurant.json').success(function(data){
+        var tmp = [];
+         tmp = data.restaurants.filter((restaurant) => {
+          if (star === 5){
+            return (restaurant.restaurant.cuisines.includes(cuisine) && (restaurant.restaurant.user_rating.aggregate_rating === star));
+          } else {
+            return (restaurant.restaurant.cuisines.includes(cuisine) && (restaurant.restaurant.user_rating.aggregate_rating >= star && restaurant.restaurant.user_rating.aggregate_rating < (star + 1)));
+          }
         });
-      } else {
+        debugger;
+        $scope.restaurants = tmp;
+      });
+    }
+
+    $scope.findPlace = function(name){
+      var res;
+      var inputValueCuisine = ($scope.selectedItem ? $scope.selectedItem.name : "");
+      var selectedStar = parseInt($scope.selectedStar ? $scope.selectedStar : 0);
+      var inputStar = $scope.selectedStar;
+      if (inputValueCuisine !== 'All'){
+        switch (selectedStar) {
+          case 5:
+            // debugger
+            helper(inputValueCuisine, selectedStar);
+            // $scope.restaurants = [];
+            break;
+          case 4:
+            helper(inputValueCuisine, selectedStar);
+            break;
+          case 3:
+            // debugger
+            helper(inputValueCuisine, selectedStar);
+            // debugger
+            break;
+          case 2:
+            helper(inputValueCuisine, selectedStar);
+            break;
+          default:
+          $http.get('../data/restaurant.json').success(function(data){
+            $scope.restaurants = data.restaurants.filter((restaurant) => {
+              return restaurant.restaurant.cuisines.includes(inputValueCuisine);
+            });
+          });
+
+        }
+        // $http.get('../data/restaurant.json').success(function(data){
+        //   $scope.restaurants = data.restaurants.filter((restaurant) => {
+        //     return restaurant.restaurant.cuisines.includes(inputValueCuisine);
+        //   });
+        // });
+      }
+
+      else {
         $http.get('../data/restaurant.json').success(function(data){
           $scope.restaurants = data.restaurants;
           $scope.cuisines = (data.restaurants.map(function(restaurant){
-            return {id: restaurant.restaurant.id, name: restaurant.restaurant.cuisines}
-          }))
+            return {id: restaurant.restaurant.id, name: restaurant.restaurant.cuisines};
+          }));
           $scope.cuisines.push({id: 1, name: 'All'});
         });
       }
       // return $scope.restaurants;
     };
+
   }]);
